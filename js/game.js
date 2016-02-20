f238210a58237515b52141a6a3ccde7e7fff2ee6
Maarten <maartenh@gmail.com>
2016-02-20 10:41:14 +0100

    new way of calculating total, wierd difference in methods

--- js/game.js
+++ js/game.js
@@ -32,33 +32,47 @@ function Post (nr){
   this.owner = null;
   this.dom = null;
   this.eventStack = new Array();
+  this.totalsTable = new Array();
+
   this.GetCaptured = function(team){
+    var previousCapture = this.eventStack[this.eventStack.length - 2];
+    if(previousCapture){
+      previousCapture.team.UpdateNrOfPostsOwned();
+      this.UpdateTotalForTeam(previousCapture.team);
+    }
+
     this.dom.css('background-color', selectedTeam.colour);
     this.owner = team;
     this.eventStack.push(new CaptureEvent(team, Date.now()));
     this.dom.children(".ownedBy").text("is nu van: " + team.name);
     team.UpdateNrOfPostsOwned();
-    var previousCapture = this.eventStack[this.eventStack.length - 2];
-    if(previousCapture)
-      previousCapture.team.UpdateNrOfPostsOwned();
+
+    this.EnsureEntryInTotalsTable(team);
   }
+
   this.GetCurrentTimer = function() {
     if(this.owner)
       return Date.now() - this.eventStack[this.eventStack.length - 1].timestamp;
     else
       return 0;
   }
-  this.UpdateTimer = function(){
-    var temp = this.GetCurrentTimer();
-    var minutes = Math.floor(temp/60000);
-    temp %= 60000;
-    var seconds = Math.floor(temp/1000);
-    var pseconds = ("0" + seconds).slice(-2);
-    temp %= 1000;
-    var msecs = Math.floor(temp / 100);
-    this.dom.children(".timer").text(minutes + ":" + pseconds + ":" + msecs);
-  }
+
   this.GetTotalTimeForTeam = function(team){
+    var savedVal = this.totalsTable[team];
+    var total = 0;
+
+    if(savedVal)
+      total = savedVal;
+    else
+      total = 0;
+
+    if(this.owner === team)
+      total += this.GetCurrentTimer();
+
+    return total;
+  }
+
+  this.GetTotalTimeForTeamBackup = function(team){
     var total = 0;
     for(var i = 0; i<this.eventStack.length-1; i++){
       if(this.eventStack[i].team === team){
@@ -69,6 +83,29 @@ function Post (nr){
       total += this.GetCurrentTimer();
     }
     return total;
+  }
+
+  this.EnsureEntryInTotalsTable = function(team){
+    if(!this.totalsTable[team]){
+      this.totalsTable[team] = 0;
+      console.log("ensureentries" + this.totalsTable[team]);
+    }
+  }
+
+  this.UpdateTotalForTeam = function(team){
+    this.totalsTable[team] += this.GetCurrentTimer();
+    console.log("updatetotal: " + this.totalsTable[team]);
+  }
+
+  this.UpdateTimer = function(){
+    var temp = this.GetCurrentTimer();
+    var minutes = Math.floor(temp/60000);
+    temp %= 60000;
+    var seconds = Math.floor(temp/1000);
+    var pseconds = ("0" + seconds).slice(-2);
+    temp %= 1000;
+    var msecs = Math.floor(temp / 100);
+    this.dom.children(".timer").text(minutes + ":" + pseconds + ":" + msecs);
   }
 
 }
